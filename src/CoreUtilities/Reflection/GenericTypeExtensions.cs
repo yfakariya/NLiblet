5895cbc49f0de48dd3ea35ea6d446c0c7eccee0f
yfakariya <yfakariya@gmail.com>
2011-06-28 18:51:36 +0900

    Add GenericTypeExtensions and its test.

---
+++ src/CoreUtilities/Reflection/GenericTypeExtensions.cs
@@ -0,0 +1,135 @@
+﻿#region -- License Terms --
+//
+// NLiblet
+//
+// Copyright (C) 2011 FUJIWARA, Yusuke
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+#endregion -- License Terms --
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace NLiblet.Reflection
+{
+	/// <summary>
+	///		Define utility extension method for generic type.
+	/// </summary>
+	public static class GenericTypeExtensions
+	{
+		/// <summary>
+		///		Determine whether source type inherits directly or indirectly from specified generic type or its built type.
+		/// </summary>
+		/// <param name="source">Target type.</param>
+		/// <param name="genericTypeDefinition">Generic type definition.</param>
+		/// <returns>
+		///		<c>true</c> if <paramref name="source"/>, directly or indirectly, inherits <paramref name="genericTypeDefinition"/>,
+		///		or built closed generic interface type;
+		///		otherwise <c>false</c>.
+		/// </returns>
+		public static bool Inherits( this Type source, Type genericTypeDefinition )
+		{
+			Contract.Requires<ArgumentNullException>( source != null );
+			Contract.Requires<ArgumentNullException>( genericTypeDefinition != null );
+			Contract.Requires<ArgumentException>( genericTypeDefinition.IsGenericTypeDefinition );
+
+			for ( Type current = source; current != null; current = current.BaseType )
+			{
+				if ( current.IsGenericType )
+				{
+					var definition = current.IsGenericTypeDefinition ? current : current.GetGenericTypeDefinition();
+					if ( definition.TypeHandle.Equals( genericTypeDefinition.TypeHandle ) )
+					{
+						return true;
+					}
+				}
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		///		Determine whether source type implements specified generic type or its built type.
+		/// </summary>
+		/// <param name="source">Target type.</param>
+		/// <param name="genericTypeDefinition">Generic interface type definition.</param>
+		/// <returns>
+		///		<c>true</c> if <paramref name="source"/> implements <paramref name="genericTypeDefinition"/>,
+		///		or built closed generic interface type;
+		///		otherwise <c>false</c>.
+		/// </returns>
+		public static bool Implements( this Type source, Type genericTypeDefinition )
+		{
+			Contract.Requires<ArgumentNullException>( source != null );
+			Contract.Requires<ArgumentNullException>( genericTypeDefinition != null );
+			Contract.Requires<ArgumentException>( genericTypeDefinition.IsInterface );
+			Contract.Requires<ArgumentException>( genericTypeDefinition.IsGenericTypeDefinition );
+
+			return
+				source.GetInterfaces()
+				.Where( @interface => @interface.IsGenericType )
+				.Select( @interface => @interface.GetGenericTypeDefinition() )
+				.Any( definition => definition.TypeHandle.Equals( genericTypeDefinition.TypeHandle ) );
+		}
+
+		/// <summary>
+		///		Get name of type without namespace and assembly name of itself and its generic arguments.
+		/// </summary>
+		/// <param name="source">Target type.</param>
+		/// <returns>Simple name of type.</returns>
+		public static string GetName( this Type source )
+		{
+			Contract.Requires<ArgumentNullException>( source != null );
+			if ( !source.IsGenericType )
+			{
+				return source.Name;
+			}
+
+			return
+				String.Join(
+					String.Empty,
+					source.Name,
+					'[',
+					String.Join( ", ", source.GetGenericArguments().Select( t => t.GetName() ) ),
+					']'
+				);
+		}
+
+		/// <summary>
+		///		Get full name of type including namespace and excluding assembly name of itself and its generic arguments.
+		/// </summary>
+		/// <param name="source">Target type.</param>
+		/// <returns>Full name of type.</returns>
+		public static string GetFullName( this Type source )
+		{
+			Contract.Requires<ArgumentNullException>( source != null );
+			if ( !source.IsGenericType )
+			{
+				return source.FullName;
+			}
+
+			return 
+				String.Join( 
+					String.Empty,
+					source.Namespace,
+					Type.Delimiter,
+					source.Name,
+					'[',
+					String.Join( ", ", source.GetGenericArguments().Select( t => t.GetFullName() ) ),
+					']'
+				);
+		}
+	}
+}
---
+++ test/CoreUtilities.Test/Reflection/GenericTypeExtensionsTest.cs
@@ -0,0 +1,114 @@
+﻿#region -- License Terms --
+//
+// NLiblet
+//
+// Copyright (C) 2011 FUJIWARA, Yusuke
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+#endregion -- License Terms --
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace NLiblet.Reflection
+{
+	[TestFixture]
+	public class GenericTypeExtensionsTest
+	{
+		[Test]
+		public void TestGetName()
+		{
+			Assert.AreEqual( "Object", typeof( object ).GetName() );
+			Assert.AreEqual( "Object[]", typeof( object[] ).GetName() );
+			Assert.AreEqual(
+				"Dictionary`2[String, Object]",
+				typeof( Dictionary<string, object> ).GetName()
+			);
+			Assert.AreEqual(
+				"Func`2[IEnumerable`1[FileInfo], IDictionary`2[String, Int64]]",
+				typeof( Func<IEnumerable<FileInfo>, IDictionary<string, long>> ).GetName()
+			);
+		}
+		
+		[Test]
+		public void TestGetFullName()
+		{
+			Assert.AreEqual( "System.Object", typeof( object ).GetFullName() );
+			Assert.AreEqual( "System.Object[]", typeof( object[] ).GetFullName() );
+			Assert.AreEqual( 
+				"System.Collections.Generic.Dictionary`2[System.String, System.Object]", 
+				typeof( Dictionary<string, object> ).GetFullName() 
+			);
+			Assert.AreEqual(
+				"System.Func`2[System.Collections.Generic.IEnumerable`1[System.IO.FileInfo], System.Collections.Generic.IDictionary`2[System.String, System.Int64]]",
+				typeof( Func<IEnumerable<FileInfo>, IDictionary<string, long>> ).GetFullName()
+			);
+		}
+
+		[Test]
+		public void TestInherits()
+		{
+			Assert.IsTrue( typeof( SomeClassDirect ).Inherits( typeof( EqualityComparer<> ) ) );
+			Assert.IsTrue( typeof( SomeClassIndirect ).Inherits( typeof( EqualityComparer<> ) ) );
+			Assert.IsFalse( typeof( SomeClassDirect ).Inherits( typeof( IEqualityComparer<> ) ) );
+			Assert.IsFalse( typeof( SomeClassDirect ).Inherits( typeof( IEqualityComparer<> ) ) );
+			Assert.IsFalse( typeof( ISomeInterfaceDirect ).Inherits( typeof( IEqualityComparer<> ) ) );
+			Assert.IsFalse( typeof( ISomeInterfaceIndirect ).Inherits( typeof( IEqualityComparer<> ) ) );
+		}
+
+		[Test]
+		public void TestImplements()
+		{
+			Assert.IsTrue( typeof( SomeClassDirect ).Implements( typeof( IEqualityComparer<> ) ) );
+			Assert.IsTrue( typeof( SomeClassDirect ).Implements( typeof( IEqualityComparer<> ) ) );
+			Assert.IsTrue( typeof( ISomeInterfaceDirect ).Implements( typeof( IEqualityComparer<> ) ) );
+			Assert.IsTrue( typeof( ISomeInterfaceIndirect ).Implements( typeof( IEqualityComparer<> ) ) );
+		}
+
+		private class SomeClassDirect : EqualityComparer<string>
+		{
+			public override bool Equals( string x, string y )
+			{
+				throw new NotImplementedException();
+			}
+
+			public override int GetHashCode( string obj )
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		private abstract class SomeClassBase<T> : EqualityComparer<T> { }
+
+		private class SomeClassIndirect : SomeClassBase<string>
+		{
+			public override bool Equals( string x, string y )
+			{
+				throw new NotImplementedException();
+			}
+
+			public override int GetHashCode( string obj )
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		private interface ISomeInterfaceDirect : IEqualityComparer<string> { }
+		private interface ISomeInterfaceBase<T> : IEqualityComparer<T> { }
+		private interface ISomeInterfaceIndirect : ISomeInterfaceBase<string> { }
+
+	}
+}
