2d2e8dacd14c988d08a3c652b4276028dca27a48
yfakariya <yfakariya@gmail.com>
2011-07-31 01:18:33 +0900

    code cleanup

--- src/CoreUtilities/Text/Formatters/SerializationInfoFormatter.cs
+++ src/CoreUtilities/Text/Formatters/SerializationInfoFormatter.cs
@@ -19,11 +19,8 @@
 #endregion -- License Terms --
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Runtime.Serialization;
 using System.Diagnostics;
+using System.Runtime.Serialization;
 
 namespace NLiblet.Text.Formatters
 {
