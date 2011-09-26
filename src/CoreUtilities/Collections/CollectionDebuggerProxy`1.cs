e6f91b123678a16941f85f4993385f75e71d6927
yfakariya <yfakariya@gmail.com>
2011-09-26 20:59:28 +0900

    Change DebuggerProxy of collections Mono implementation to porting implementation.

---
+++ src/CoreUtilities/Collections/CollectionDebuggerProxy`1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace NLiblet.Collections
+{
+	// Thanks to Mono implementation.
+
+	/// <summary>
+	///		Debugger type proxy for <see cref="ICollection{T}"/>.
+	/// </summary>
+	/// <typeparam name="T">The element type of the collection.</typeparam>
+	internal sealed class CollectionDebuggerProxy<T>
+	{
+		private readonly ICollection<T> _collection;
+
+		public CollectionDebuggerProxy(ICollection<T> target)
+		{
+			this._collection = target;
+		}
+
+		[DebuggerBrowsable( DebuggerBrowsableState.RootHidden )]
+		public T[] Items
+		{
+			get
+			{
+				if ( this._collection == null )
+				{
+					return null;
+				}
+
+				var result = new T[ this._collection.Count ];
+				this._collection.CopyTo( result, 0 );
+				return result;
+			}
+		}
+	}
+}
---
+++ src/CoreUtilities/Collections/DictionaryDebuggerProxy`2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace NLiblet.Collections
+{
+	// Thanks to Mono implementation.
+
+	/// <summary>
+	///		Debugger type proxy for <see cref="IDictionary{TKey,TValue}"/>.
+	/// </summary>
+	/// <typeparam name="TKey">The key type of the dictionary.</typeparam>
+	/// <typeparam name="TValue">The value type of the dictionary.</typeparam>
+	internal sealed class DictionaryDebuggerProxy<TKey,TValue>
+	{
+		private readonly IDictionary<TKey,TValue> _dictionary;
+
+		public DictionaryDebuggerProxy( IDictionary<TKey, TValue> target )
+		{
+			this._dictionary = target;
+		}
+
+		[DebuggerBrowsable( DebuggerBrowsableState.RootHidden )]
+		public KeyValuePair<TKey,TValue>[] Items
+		{
+			get
+			{
+				if ( this._dictionary == null )
+				{
+					return null;
+				}
+
+				var result = new KeyValuePair<TKey, TValue>[ this._dictionary.Count ];
+				this._dictionary.CopyTo( result, 0 );
+				return result;
+			}
+		}
+	}
+}
